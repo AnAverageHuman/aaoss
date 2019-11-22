a4dfaf22a6c1c41d3ed13efda697914835fa3b86
Jeffrey Lin <jeffrey@icurse.nl>
2019-11-21 20:04:52 -0500

    display.c: add skeleton for display command

--- src/display.c
+++ src/display.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
 #include "aaoss.h"
 
-void show(const char *op) {}
+static void show_die(const char *op) {
+  fprintf(stderr, "Unknown argument %s to S command.\n", op);
+}
+
+void show(const char *op) {
+  if (op[1]) { // more than one character argument, abort
+    show_die(op);
+    return;
+  }
+
+  switch (*op) {
+  case 'r':
+    break;
+  case 'i':
+    break;
+  case 'm':
+    break;
+  default:
+    show_die(op);
+  }
+}
