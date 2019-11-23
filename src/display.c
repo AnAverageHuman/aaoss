0b679fcb1377d2470d089fe8ff4f4d933c4dbca4
Jeffrey Lin <jeffrey@icurse.nl>
2019-11-22 23:29:28 -0500

    display.c: add function to show processes

--- src/display.c
+++ src/display.c
@@ -6,7 +6,18 @@ static void show_die(const char *op) {
   fprintf(stderr, "Unknown argument %s to S command.\n", op);
 }
 
-void show(const char *op) {
+static void show_processes(const struct process *const *pcb) {
+  if (*pcb) {
+    printf("  %6s %6s %2c\n", "PID", "PR", 'S');
+    process_show(*pcb, 'R');
+
+    for (struct process *it = (*pcb)->next; it; it = it->next) {
+      process_show(it, 'S');
+    }
+  }
+}
+
+void show(const struct process *const *pcb, const char *op) {
   if (op[1]) { // more than one character argument, abort
     show_die(op);
     return;
@@ -14,6 +25,7 @@ void show(const char *op) {
 
   switch (*op) {
   case 'r':
+    show_processes(pcb);
     break;
   case 'i':
     break;
