6aeae5e9bca20a9ad49e01f0f90bf9db686b00d2
Jeffrey Lin <jeffrey@icurse.nl>
2019-11-23 22:55:13 -0500

    disk.c: add disks creation and deletion

--- include/disk.h
+++ include/disk.h
@@ -1,6 +1,11 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include <sys/types.h>
+
+struct process **disks_create(const size_t);
+void disks_destroy(struct process **, const size_t);
+
 void disk_request(const long int, const char *);
 void disk_done(const long int);
 
--- src/disk.c
+++ src/disk.c
@@ -1,4 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "disk.h"
+#include "process.h"
+
+struct process **disks_create(const size_t numdisks) {
+  struct process **disks;
+  size_t memsize = numdisks * sizeof *disks;
+  disks = malloc(memsize);
+  return memset(disks, 0, memsize);
+}
+
+void disks_destroy(struct process **disks, const size_t numdisks) {
+  for (size_t i = 0; i < numdisks; i++) {
+    while (disks[i]) {
+      process_exit(&disks[i]);
+    }
+  }
+
+  free(disks);
+}
 
 void disk_request(long int disk, const char *filename) {}
 
